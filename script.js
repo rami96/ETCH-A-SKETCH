21d05da9a4a6f1bdd0aba1da56cfc6f5eaa59f8a
Saifeddine Rami <sifeddinerami96@gmail.com>
2020-11-01 05:05:30 +0100

    fixed some bugs and added minor css

--- script.js
+++ script.js
@@ -1,68 +1,52 @@
+//To switch between modes (when you enter vs when you press clear)
 let press = true;
 let conte;
 creates=(side)=>{
+    //Create grid-template-columns for rows
     let res = "";
     for(let a = 0; a < side; a++){
         res+="auto "
     }
     if(press = true){
-            //Create Container for smaller divs and assign id to style it
-    //const cont = document.getElementsByClassName("cont");
-    //var conte = document.getElementsByClassName("conte");
     conte = document.createElement("div");
     conte.className = "conte"
     document.body.appendChild(conte);
-    //conte.appendChild(document.createTextNode('top div'));
 
-
-    //console.log(conte)
-    //alert("Yooo");
-    //Create 60 divs
+    //Create rows and assign them to the container "conte"
     for (let a = 0; a < side; a++){
         let row = document.createElement("div");
         conte.append(row);
         row.id = "row"+a;
         row.style.height = "400/side";
-        row.style.display = "grid";
-        
+        row.style.display = "grid";        
         row.style.gridTemplateColumns = res;
-        //row.style.border = "1px solid black";
-
         for(let b=0; b < side; b++){
+            //Create columns and assign them to rowa
             let smalldiv = document.createElement("div");
-            //smalldiv.id = 'c'+a;
-            //smalldiv.className = "small";
             let cls = document.getElementById("row"+a);
             cls.appendChild(smalldiv);
+            //Style columns
             smalldiv.style.width = "400/side";
-            //smalldiv.style.backgroundColor = "red"
             smalldiv.style.border = "1px solid black";
-
+            //Change color to random color
             smalldiv.addEventListener("mouseenter",function(){
                 this.style.backgroundColor= "rgb("+Math.floor(Math.random()*255)+","+
                 Math.floor(Math.random()*255)+","+
                 Math.floor(Math.random()*255)+")";})
         }
-
-        /*for(let b=0; a < side; b++){
-            let smalldiv = document.createElement("div");
-            smalldiv.id = 'c'+a;
-            smalldiv.className = "small";
-            row.appendChild(smalldiv);
-            smalldiv.addEventListener("mouseenter",function(){
-                this.style.backgroundColor= "rgb("+Math.floor(Math.random()*255)+","+
-                Math.floor(Math.random()*255)+","+
-                Math.floor(Math.random()*255)+")";})
-        }*/
     }
     }
 }
 creates(10)
-//}
-//cont.innerHTML+='<div class="small"></div>';
+
 //Make clear and restart button
 clearit=()=>{
-       let sides = prompt("Enter the number of sides:");
+       //let sides = prompt("Enter the number of sides: must be 100 or less");
+       let sides;
+       do{
+        sides = prompt("Enter the number of sides:must be 100 or less");
+       }
+       while(sides > 100);
        press = false;
        conte.remove();
        creates(sides);
